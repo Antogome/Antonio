4121a4b49d110177e80cfc8cc06299259d99b67c
Antogome <59769394+Antogome@users.noreply.github.com>
2023-03-08 11:50:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Obtener la etiqueta de encabezado
 const header = document.querySelector('header');
 
-// Agregar un evento de clic al encabezado
-header.addEventListener('click', () => {
-  // Cambiar el color de fondo del encabezado
-  header.style.backgroundColor = getRandomColor();
+// Obtener la tabla del correo electrónico
+const emailTable = document.querySelector('.Antonio3');
+
+// Agregar un evento de clic a la tabla del correo electrónico
+emailTable.addEventListener('click', () => {
+  // Cambiar el color de fondo de la página
+  document.body.style.backgroundColor = getRandomColor();
 });
 
 // Función para generar un color aleatorio
